5d9b25a5c9ae93ba7f7a299bee07610a37d5fb07
ainokuos <aino.kuosmanen@helsinki.fi>
2021-11-20 18:26:30 +0200

    labyrintti ja testaus

--- src/labyrintti.py
+++ src/labyrintti.py
@@ -1,50 +1,71 @@
 import random
 
-def labyrintti():
-    """Luo satunnaisen labyrintin"""
-    ruudukko = [[]]*10
-    vierailtu = [[]]*10
-    for i in range (10):
-        ruudukko[i]=["0"]*10
-        vierailtu[i]=[0]*10
-    
-    def luo(x, y):
-        """Luo rekursiivisesti labyrintin ruudukkoon"""
-        if y<0 or x<0 or y>=10 or x>=10:
-            return
-        if vierailtu[x][y] == 1:
-            return 
-        vierailtu[x][y] = 1
-        if ruudukko[x][y] == "#":
-            return
+class Labyrintti:
+    def __init__(self, n):
+        self.n = n
+        self.ruudukko = []
+        self.vierailtu = []
+        self.edelliset = []
 
+    def luo_ruudukko(self):
+        self.ruudukko = [[]]*self.n
+        self.vierailtu = [[]]*self.n
+
+        for i in range(self.n):
+            self.ruudukko[i] = ["#"]*self.n
+            self.vierailtu[i] = [0]*self.n
+        
+        self.ruudukko[1][1] = "."
+
+
+    def viereiset(self, x, y):
         viereiset = []
-        if y-1 >= 0:
-            yla = (x, y-1)
-            if vierailtu[x][y-1] == 0:
+
+        if y-2 >= 0:
+            yla = (x, y-1, x, y-2)
+            if self.vierailtu[x][y-2] == 0:
                 viereiset.append(yla)
-        if y+1 < 10:
-            ala = (x, y+1)
-            if vierailtu[x][y+1] == 0:
+
+        if y+2 < self.n-1:
+            ala = (x, y+1, x, y+2)
+            if self.vierailtu[x][y+1] == 0:
                 viereiset.append(ala)
-        if x+1 < 10:
-            oik = (x+1, y)
-            viereiset.append(oik)
-        if x-1 >= 0:
-            vas = (x-1, y)
-            viereiset.append(vas)
 
+        if x+2 < self.n-1:
+            oik = (x+1, y, x+2, y)
+            if self.vierailtu[x+2][y] == 0:
+                viereiset.append(oik)
+
+        if x-2 >= 0:
+            vas = (x-1, y, x-2, y)
+            if self.vierailtu[x-2][y] == 0:
+                viereiset.append(vas)
+        return viereiset
+        
+    def luo_labyrintti(self, x, y):
+        viereiset = self.viereiset(x, y)
+        self.vierailtu[x][y] = 1
+
+        if len(viereiset) == 0:
+            if len(self.edelliset) == 0:
+                return
+            edellinen = self.edelliset.pop(-1)
+            self.luo_labyrintti(edellinen[0], edellinen[1])
+        
+        if len(viereiset) > 1:
+            self.edelliset.append((x, y))
+
+        if len(viereiset) == 0:
+            return
         reitti = random.choice(viereiset)
-        ruudukko[reitti[0]][reitti[1]] = "."
-        for k in viereiset:
-            if k != reitti:
-                ruudukko[k[0]][k[1]] = "#"
-        luo(reitti[0], reitti[1])
 
-
+        self.ruudukko[reitti[0]][reitti[1]] = "."
+        self.ruudukko[reitti[2]][reitti[3]] = "."
+        
+        self.luo_labyrintti(reitti[2], reitti[3])
     
-    luo(0, 0)
-    for k in ruudukko:
-        print(k)
-
-labyrintti()
+    def piirra(self):
+        self.luo_ruudukko()
+        self.luo_labyrintti(1, 1)
+        for k in self.ruudukko:
+            print(k)
--- src/tests/labyrintti_test.py
+++ src/tests/labyrintti_test.py
@@ -1,2 +1,18 @@
 import unittest
+from labyrintti import Labyrintti
+
+class TestLabyrintti(unittest.TestCase):
+    def setUp(self):
+        self.labyrintti = Labyrintti(10)
+    
+    def test_luo_ruudukko(self):
+        self.labyrintti.luo_ruudukko()
+
+        self.assertEqual(len(self.labyrintti.ruudukko), 10)
+    
+    def test_luo_labyrintti(self):
+        self.labyrintti.luo_ruudukko()
+        self.labyrintti.luo_labyrintti(1, 1)
+
+        self.assertEqual(len(self.labyrintti.ruudukko), 10)
 
