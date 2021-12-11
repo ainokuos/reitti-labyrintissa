ab85422a9eb3b0e90a1b3084d1d90585d68fb0c0
ainokuos <aino.kuosmanen@helsinki.fi>
2021-12-11 22:55:56 +0200

    gitignore

--- src/tests/dead_end_filling_test.py
+++ src/tests/dead_end_filling_test.py
@@ -3,21 +3,21 @@ from dead_end_filling import DeadEndFilling
 
 class TestDeadEndFilling(unittest.TestCase):
     def setUp(self):
-        self.labyrintti =  [['#', '.', '#', '#', '#'], 
-                            ['#', '.', '#', '.', '#'], 
-                            ['#', '.', '#', '.', '#'], 
-                            ['#', '.', '.', '.', '#'], 
+        self.labyrintti =  [['#', '.', '#', '#', '#'],
+                            ['#', '.', '#', '.', '#'],
+                            ['#', '.', '#', '.', '#'],
+                            ['#', '.', '.', '.', '#'],
                             ['#', '#', '#', '.', '#']]
         self.DEF = DeadEndFilling(self.labyrintti)
-    
+
     def test_umpikujat(self):
         self.DEF.umpikujat()
         self.assertEqual(self.labyrintti[1][3], "#")
-    
+
     def test_muuraa(self):
         self.DEF.muuraa(1, 3)
         self.assertEqual(self.labyrintti[2][3], "#")
-    
+
     def test_palauta(self):
         self.DEF.palauta()
         self.assertEqual(self.labyrintti[3][3], ".")
