d8d416969d072eed16cbc6fec4ec4694dc0223fb
ainokuos <aino.kuosmanen@helsinki.fi>
2021-12-28 11:30:21 +0200

    testaus

--- src/tests/breadth_first_search_test.py
+++ src/tests/breadth_first_search_test.py
@@ -8,8 +8,39 @@ class TestBreadthFirstSearch(unittest.TestCase):
                             ['#', '.', '#', '.', '#'],
                             ['#', '.', '.', '.', '.'],
                             ['#', '#', '#', '#', '#']]
+        
+        self.labyrintti2 = [['#', '.', '#', '#', '#', '#'],
+                            ['#', '.', '#', '#', '.', '#'], 
+                            ['#', '.', '.', '#', '.', '.'], 
+                            ['#', '#', '.', '#', '.', '#'], 
+                            ['#', '.', '.', '.', '.', '#'], 
+                            ['#', '#', '#', '#', '#', '#']]
+
         self.BFS = BreadthFirstSearch(self.labyrintti)
+        self.BFS2 = BreadthFirstSearch(self.labyrintti2)
 
     def test_palauta(self):
         self.BFS.palauta()
         self.assertEqual(self.labyrintti[3][1], "x")
+    
+    def test_toinen_labyrintti(self):
+        self.BFS2.palauta()
+        self.assertEqual(self.labyrintti2[1][1], "x")
+    
+    def test_oikea_reitti(self):
+        self.BFS.palauta()
+        self.assertEqual(self.labyrintti[1][3], ".")
+        self.assertEqual(self.labyrintti[3][4], "x")
+    
+    def test_haku(self):
+        self.BFS.haku()
+        self.assertEqual(self.BFS.etaisyys[3][4], 6)
+    
+    def test_oikea_vastaus(self):
+        vastaus = [['#', 'x', '#', '#', '#'],
+                    ['#', 'x', '#', '.', '#'],
+                    ['#', 'x', '#', '.', '#'],
+                    ['#', 'x', 'x', 'x', 'x'],
+                    ['#', '#', '#', '#', '#']]
+        self.BFS.palauta()
+        self.assertEqual(self.labyrintti, vastaus)
--- src/tests/dead_end_filling_test.py
+++ src/tests/dead_end_filling_test.py
@@ -8,11 +8,25 @@ class TestDeadEndFilling(unittest.TestCase):
                             ['#', '.', '#', '.', '#'],
                             ['#', '.', '.', '.', '.'],
                             ['#', '#', '#', '#', '#']]
+
+        self.labyrintti2 = [['#', '.', '#', '#', '#', '#'],
+                            ['#', '.', '#', '#', '.', '#'], 
+                            ['#', '.', '.', '#', '.', '.'], 
+                            ['#', '#', '.', '#', '.', '#'], 
+                            ['#', '.', '.', '.', '.', '#'], 
+                            ['#', '#', '#', '#', '#', '#']]
+
         self.DEF = DeadEndFilling(self.labyrintti)
+        self.DEF2 = DeadEndFilling(self.labyrintti2)
 
     def test_umpikujat(self):
         self.DEF.umpikujat()
         self.assertEqual(self.labyrintti[1][3], "#")
+    
+    def test_toinen_labyrintti_umpikujat(self):
+        self.DEF2.umpikujat()
+        self.assertEqual(self.labyrintti[4][1], "#")
+        self.assertEqual(self.labyrintti[1][4], "#")
 
     def test_muuraa(self):
         self.DEF.muuraa(1, 3)
@@ -22,3 +36,17 @@ class TestDeadEndFilling(unittest.TestCase):
         self.DEF.palauta()
         self.assertEqual(self.labyrintti[3][3], ".")
         self.assertEqual(self.labyrintti[1][3], "#")
+    
+    def test_palauta_toinen_labyrintti(self):
+        self.DEF2.palauta()
+        self.assertEqual(self.labyrintti2[2][4], ".")
+        self.assertEqual(self.labyrintti2[1][4], "#")
+
+    def test_oikea_vastaus(self):
+        vastaus = [['#', '.', '#', '#', '#'],
+                    ['#', '.', '#', '#', '#'],
+                    ['#', '.', '#', '#', '#'],
+                    ['#', '.', '.', '.', '.'],
+                    ['#', '#', '#', '#', '#']]
+        self.DEF.palauta()
+        self.assertEqual(self.labyrintti, vastaus)
